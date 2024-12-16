b46dcb706f35a6e88c0f7cb56ae6de22095805d6
Denis <ndenis@cps.k12.il.us>
2024-12-16 11:26:59 -0600

    yes king

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
-st.write("Hello, World!")
+st.write('yes king')
 
 st.title("are we deaduzz?")
-st.header("HAWK TUAH")
+st.header("MANGO MANGO MANGO")
 st.subheader("Spit on that thang")
