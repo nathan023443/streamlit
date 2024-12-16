2dbe5d66163c7cbe62620ca751a897a8538b4f51
Denis <ndenis@cps.k12.il.us>
2024-12-16 11:42:51 -0600

    made a lot of changes

--- main.py
+++ main.py
@@ -10,14 +10,17 @@ st.header("user information users")
 # Text input for name
 name= st.text_input('Enter your name')
 # Dropdown menu for selecting an option
-
-# Slider for selecting a value
 option=['option 1', 'option 2' , 'option 3']
-selected_option= st.selections('choose an option', option)
+selected_option= st.selectbox('choose an option', option)
+# Slider for selecting a value
+slider_value= st.slider('select a value',1, 100, 1000)
+
 # Submit button
 if st.button('submit'):
     st.write(f'Name: {name}')
-
+    st.write(f'selected option:{selected_option}')
+    st.write(f'Slider ValueL {slider_value}')
 # Additional information
-
+st.subheader('Summary')
+st.write('Fill out the form above and click the submit button')
 # Footer
